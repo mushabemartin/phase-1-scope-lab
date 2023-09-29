786d245fee9c0bd94c5ac75f76c4d915bcd281df
MUSHABE <mushabe.martin@student.moringaschool.com>
2023-09-29 11:33:25 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(newFavorite) {
+  bestCustomer = newFavorite;
+}
+
+const leastFavoriteCustomer = 'unwanted';
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'unwanted'; // This will throw an error as you cannot reassign a constant.
+}
